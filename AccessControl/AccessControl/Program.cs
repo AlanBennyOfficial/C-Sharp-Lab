596e8f2003e8ad299c3880ee08f96685e40f7289
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-07 10:47:28 +0530

    Add files via upload

---
+++ AccessControl/AccessControl/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+//using System.Diagnostics.CodeAnalysis;
+
+//class Age
+//{
+//    private int age;
+//    public void ShowAge(int age)
+//    {
+//        Console.WriteLine("Age = " + age);
+//    }
+//}
+//class display
+//{
+//    static void Main(string[] args) { 
+//        Age a = new Age();
+//        a.ShowAge(23);
+//    }
+//}
+
+class Animal
+{
+    protected void MakeSound()
+    {
+        Console.WriteLine("Animal sounds...");
+    }
+}
+
+class Dog : Animal
+{
+    public void Bark()
+    {
+        MakeSound();
+        Console.WriteLine("Dog Sounds...");
+    }
+}
+class display
+{
+    static void Main(string[] args)
+    {
+        Dog d = new Dog();
+        d.Bark();
+    }
+}
