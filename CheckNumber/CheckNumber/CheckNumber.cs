fd2ebe50998e9b525c7485723555d9596eaa93ce
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-07 10:50:18 +0530

    Add files via upload

---
+++ CheckNumber/CheckNumber/CheckNumber.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckNumber
+{
+    public class CheckNumber
+    {
+        public void Determine(int number)
+        {
+
+            if (number > 0)
+            {
+                Console.WriteLine(number + " is Positive");
+            }
+            else if (number < 0)
+            {
+                Console.WriteLine(number + " is Negative");
+            }
+            else
+            {
+                Console.WriteLine("The Number is Zero. ");
+            }
+
+        }
+
+    }
+}
---
+++ CheckNumber/CheckNumber/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace CheckNumber
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            CheckNumber check = new CheckNumber();
+            check.Determine(-12);
+            check.Determine(9);
+            check.Determine(0);
+        }
+    }
+}
