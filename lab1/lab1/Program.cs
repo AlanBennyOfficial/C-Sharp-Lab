18089b066bdfc69265c56a2243ac15946403e7b6
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-08 13:55:03 +0530

    Add files via upload

---
+++ lab1/lab1/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+class car
+{
+    public string color;
+    public string model;
+    public void drive()
+    {
+        Console.WriteLine("i like driving");
+    }
+    class program
+    {
+        static void Main()
+        {
+            car c = new car();
+            c.color = "red";
+            Console.WriteLine("color is"+ c.color);
+            c.drive();
+
+
+
+
+
+
+        }
+    }
+
+}
