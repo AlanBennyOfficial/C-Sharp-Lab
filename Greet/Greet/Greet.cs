c9d84393e6e8c437b9b0cb61ffb6eacbc1555fc1
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-08 13:57:28 +0530

    Add files via upload

---
+++ Greet/Greet/Greet.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Greet
+{
+    public class Greet
+    {
+        public void SayHello(string name)
+        {
+            Console.WriteLine("Hello," + name + " Have a nice day!!");
+        }
+    }
+}
