41c033c7b9183031874c7db6c32088f192997524
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-14 10:11:50 +0530

    Add files via upload

---
+++ LuckyTicketNumbers/LuckyTicketNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace LuckyTicketNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Print a list of lucky ticket numbers between 1 and 100, multiples of 4 and 6 are considered lucky.
+            Console.WriteLine("Lucky Ticket Numbers:");
+            for(int i = 1; i <= 100; i++)
+            {
+                if (i % 4 == 0 && i % 6 == 0)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        }
+    }
+}
