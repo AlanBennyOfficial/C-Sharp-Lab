076214a48854c77c73d67920201c55b43d61da45
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-14 10:53:12 +0530

    Add files via upload

---
+++ AbstractCalculateShapeArea/AbstractCalculateShapeArea/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace AbstractCalculateShapeArea
+{
+    using System;
+    abstract class Shape
+    {
+        abstract public double CalculateArea();
+    }
+    abstract class Circle : Shape
+    {
+        public double radius;
+        public override double CalcuteArea(double radius)
+        {
+            return Math.PI * radius * radius;
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Shape c1 = new Circle();
+            Console.WriteLine($"Area of the circle: {c1.CalculateArea(5.0)}");
+            
+        }
+    }
+}
