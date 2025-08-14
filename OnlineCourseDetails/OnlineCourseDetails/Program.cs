d5929dc0b3c258b2bec4f7476e87b73425e17af4
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-14 10:20:47 +0530

    Add files via upload

---
+++ OnlineCourseDetails/OnlineCourseDetails/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace OnlineCourseDetails
+{
+    class Study
+    {
+        protected void CourseDetails()
+        {
+            Console.WriteLine("This is a course on C# programming.");
+        }
+    }
+    class OnlineCourse : Study
+    {
+        public void CourseInfo() 
+        { 
+            CourseDetails();
+            Console.WriteLine("This course is available online.");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            OnlineCourse o1 = new OnlineCourse();
+            o1.CourseInfo();
+        }
+    }
+}
