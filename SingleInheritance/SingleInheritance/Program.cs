5baefa796dd942aac9deac79209e2443791d01f8
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-14 10:03:57 +0530

    Add files via upload

---
+++ SingleInheritance/SingleInheritance/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace SingleInheritance
+{
+    public class Animal
+    {
+        public void Speak()
+        {
+            Console.WriteLine("Animal sounds");
+        }
+    }
+    public class Dog : Animal
+    {
+        public void Bark()
+        {
+            Console.WriteLine("Bark!");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog d1 = new Dog();
+            d1.Speak();
+            d1.Bark();
+        }
+    }
+}
