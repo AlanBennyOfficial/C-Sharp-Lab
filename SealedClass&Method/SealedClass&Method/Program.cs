c594bda5b69f2d4e33e9f54df4cb64e91da5c7b8
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-21 09:58:08 +0530

    Add files via upload

---
+++ SealedClass&Method/SealedClass&Method/Program.cs
@@ -0,0 +1,27 @@
+﻿sealed class Animal
+{
+    public string Name;
+    public int ageInMonth;
+
+    public void Move()
+    {
+        Console.WriteLine("Moving...");
+    }
+    public void Eat()
+    {
+        Console.WriteLine("Eating...");
+    }
+}
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Animal a1 = new Animal();
+        a1.Name = "Dog";
+        a1.ageInMonth = 1;
+        a1.Move();
+        a1.Eat();
+    }
+}
+
