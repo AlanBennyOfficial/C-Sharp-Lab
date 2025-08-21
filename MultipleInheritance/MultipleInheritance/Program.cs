71e73a1bc6b31b789624b4f418a09b9c24b7f5be
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-08-21 10:05:21 +0530

    Add files via upload

---
+++ MultipleInheritance/MultipleInheritance/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MultipleInheritance
+{
+    interface Cat
+    {
+        void MakeSound()
+        {
+            Console.WriteLine("Meow...");
+        }
+    }
+    interface Dog
+    {
+        void MakeSound()
+        {
+            Console.WriteLine("Bark..");
+        }
+    }
+    class Animal : Cat, Dog
+    {
+        public void MakeSound()
+        {
+            Console.WriteLine("Animal sounds...");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Animal a1 = new Animal();
+            a1.MakeSound();
+        }
+    }
+}
