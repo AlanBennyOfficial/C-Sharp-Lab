514639f1279c3e9021f97e69ac01edbb95fad3cc
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-09-04 10:09:53 +0530

    Add files via upload

---
+++ PaymentDetails/PaymentDetails/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace PaymentDetails
+{   
+    interface IPayment
+    {
+        public void MakePayment(double amount);
+    }
+    class CreditCardPayment:IPayment
+    {
+        public void MakePayment(double amount)
+        {
+            Console.WriteLine($"Payment of INR {amount} made via Credit Card");
+        }
+    }
+    class PayPalPayment:IPayment
+    {
+        public void MakePayment(double amount)
+        {
+            Console.WriteLine($"Payment of INR {amount} made via PayPal");
+        }
+    }
+    class UPIpayment : IPayment
+    {
+        public void MakePayment(double amount)
+        {
+            Console.WriteLine($"Payment of INR {amount} made via UPI");
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            CreditCardPayment c1 = new CreditCardPayment();
+            c1.MakePayment(5000.0);
+            Console.WriteLine();
+
+            PayPalPayment p1 = new PayPalPayment();
+            p1.MakePayment(2000.0);
+            Console.WriteLine() ;
+
+            UPIpayment u1 = new UPIpayment();
+            u1.MakePayment(1000.0);
+        }
+    }
+}
