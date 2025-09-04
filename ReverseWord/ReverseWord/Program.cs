d39c682a7855fa6d0bf1fb3590e5c5d2bc7f834b
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-09-04 10:35:33 +0530

    Add files via upload

---
+++ ReverseWord/ReverseWord/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ReverseWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your string: ");
+            string str = Console.ReadLine();
+
+            string[] arr = str.Split(" ").ToArray();
+            for (int i = arr.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"{arr[i]} ");
+            }
+            
+        }
+    }
+}
