f5ebc94918d25b41c9d92001d18c4d30de153e58
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-09-18 10:10:13 +0530

    Add files via upload

---
+++ NestedTryCatch/NestedTryCatch/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace NestedTryCatch
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int divisor = 0;
+            try
+            {
+                try
+                {
+                    int divideByZero = 6 / divisor;
+                }
+                catch (IndexOutOfRangeException e)
+                {
+                    Console.WriteLine("Inner catch is executed: " + e.Message);
+                }
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine("Outer catch executed: " + ex.Message);
+            }
+        }
+    }
+}
