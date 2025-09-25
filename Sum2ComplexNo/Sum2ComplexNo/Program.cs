aa92d6941f1aa7bbbc6597c2b641b9699ebee219
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-09-25 10:54:53 +0530

    Add files via upload

---
+++ Sum2ComplexNo/Sum2ComplexNo/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Sum2ComplexNo
+{
+    public class Complex
+    {
+        public int real {  get; set; }
+        public int imaginary { get; set; }
+        public Complex(int r, int i)
+        {
+            real = r;
+            imaginary = i;
+        }
+        public static Complex operator +(Complex a, Complex b)
+        {
+            return new Complex(a.real+b.real, a.imaginary+b.imaginary);
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Complex a = new Complex(10, 2);
+            Complex b = new Complex(20, 1);
+            Console.WriteLine(a + b);
+        }
+    }
+}
