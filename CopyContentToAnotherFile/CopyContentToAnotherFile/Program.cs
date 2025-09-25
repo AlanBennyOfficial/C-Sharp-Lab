106b98aa1b21bd51ef1d0ee06ea07c3a084fdd06
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-09-25 10:55:04 +0530

    Add files via upload

---
+++ CopyContentToAnotherFile/CopyContentToAnotherFile/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace CopyContentToAnotherFile
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            File.Copy("file1.txt", "file2.txt");
+            Console.WriteLine("Execution Completed");
+        }
+    }
+}
