1638436d5ab2618d3c4b89517410f5a9561e648f
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-10-09 10:45:42 +0530

    Add files via upload

---
+++ MultiCast-Delegate/MultiCast-Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+delegate void MyDelegate();
+class Program
+{
+    static void Method1()
+    {
+        Console.WriteLine("This is method 1");
+    }
+    static void Method2()
+    {
+        Console.WriteLine("This is method 2");
+    }
+    static void Main()
+    {
+        MyDelegate d= Method1;
+        d += Method2;
+        d();
+    }
+}
