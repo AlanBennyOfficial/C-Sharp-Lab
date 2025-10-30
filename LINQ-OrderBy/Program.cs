eb812220bd1b17142aa28b686d7b4d06801da40e
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-10-30 10:54:11 +0530

    Add files via upload

---
+++ LINQ-OrderBy/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQ_OrderBy
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 253, 5, 54, 53, 4365 };
+
+            var result = list.OrderBy(x => x * x);
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
