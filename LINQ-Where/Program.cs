84dd7cae0864a2ec9d881079d36e2adc0a042c5f
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-10-30 10:54:21 +0530

    Add files via upload

---
+++ LINQ-Where/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQ_Where
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1, 2, 3, 4 };
+
+            var result = list.Where(x => x>2);
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
