d4e2998ec1a36e6194ca2ef0da517706f068549c
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-10-30 10:54:17 +0530

    Add files via upload

---
+++ LINQ-Select/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace LINQ_Select
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 1,2,3,4};
+
+            var result = list.Select(x => x*x );
+
+            foreach (var r in result)
+            {
+                Console.WriteLine(r);
+            }
+        }
+    }
+}
