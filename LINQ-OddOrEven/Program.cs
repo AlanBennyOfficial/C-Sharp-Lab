55024138211aa8a2f30d38f0db3303c6c8ea03a6
Alan Benny <148381987+AlanBennyOfficial@users.noreply.github.com>
2025-10-30 10:54:07 +0530

    Add files via upload

---
+++ LINQ-OddOrEven/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace LINQ_OddOrEven
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> list = new List<int>() { 253, 5, 54, 53, 4365 };
+
+            var result = list.OrderBy(x => x * x);
+
+            foreach (var r in result)
+            {
+                if (r % 2 == 0)
+                {
+                    Console.WriteLine("Even: " + r);
+                }
+                else
+                {
+                    Console.WriteLine("Odd: " +r);
+                }
+            }
+        }
+    }
+}
